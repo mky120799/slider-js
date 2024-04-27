21fc4593b42f48ce81ac244f7c26e40251d3a390
mky120799 <mky120799@gmail.com>
2024-04-28 01:23:21 +0530

    adding the javaScript main slider logic

---
+++ Mithilesh slider/script.js
@@ -0,0 +1,14 @@
+const prev = document.querySelector(".prev");
+console.log(prev)
+const next = document.querySelector(".next");
+console.log(next)
+const slider = document.querySelector(".slider");
+console.log(slider);
+const slide = document.querySelectorAll(".slide");
+console.log(slide);
+
+// here goes the logic
+let slideIndex=0;
+next.addEventListener("click", ()=>{
+     slide[slideIndex++]='transform: translateX(-100px);'
+} )
